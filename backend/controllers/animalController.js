8bb8d67734ad90146e12b9ff315e2bf716f8a255
MartinaKostic <martina.kostic@gmail.com>
2023-09-13 02:15:08 +0200

    animal router

--- backend/controllers/animalController.js
+++ backend/controllers/animalController.js
@@ -1,23 +1,70 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from "../prisma/prismaDbClient.js";
 
-const getAnimals = async (response) => {
+const getAnimals = async (request, response) => {
   try {
     // Use Prisma's findMany to retrieve all animals from the animals table
-    const animals = await prisma.Animals.findMany();
-
+    const animals = await prisma.Animals.findMany({
+      include: {
+        species: {
+          select: {
+            animal_type: true,
+          },
+        },
+      },
+    });
+    console.log(request);
     // Send the retrieved animals as a JSON response
     response.status(200).json(animals);
   } catch (error) {
     console.error("Error retrieving animals:", error);
     response.status(500).json({ error: "Server error" });
-  } finally {
-    // Disconnect from the Prisma Client
-    await prisma.$disconnect();
   }
 };
 
+const updateAdoptionStatus = async (req, res) => {
+  const { id } = req.params;
+  const { adopted } = req.body;
+
+  try {
+    await prisma.Animals.update({
+      where: { id: parseInt(id) },
+      data: { adopted: adopted },
+    });
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error updating adopted status:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const updateAnimal = async (req, res) => {
+  const { id } = req.params;
+  const { name, years, species, adopted, checkup, chip, description } =
+    req.body;
+  console.log(req.body);
+  try {
+    await prisma.Animals.update({
+      where: { id: parseInt(id) },
+      data: {
+        name,
+        species,
+        picture,
+        years,
+        adopted,
+        checkup,
+        chip,
+        description,
+      },
+    });
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error updating animal information:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 export default {
   getAnimals,
+  updateAdoptionStatus,
+  updateAnimal,
   // Other controller functions
 };

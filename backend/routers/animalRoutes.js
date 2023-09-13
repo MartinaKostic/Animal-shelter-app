c1e5ea7b595f355e525eb0f102a3b95228a40bea
MartinaKostic <martina.kostic@gmail.com>
2023-09-13 02:15:26 +0200

    backend changes

--- backend/routers/animalRoutes.js
+++ backend/routers/animalRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 var router = express.Router();
 
 import animalController from "../controllers/animalController.js";
-router.get("/animals", animalController.getAnimals);
+router.get("/", animalController.getAnimals);
+router.patch("/:id", animalController.updateAdoptionStatus);
+router.put(":/id", animalController.updateAnimal);
 
 export default router;
